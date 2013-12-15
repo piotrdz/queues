0b06e7d74e2ff81f6063e7d452d38e32f6dff85a
rolma <rafal.olma@windowslive.com>
2013-12-15 23:15:21 +0100

    Input/output of instance file

--- src/engine/simulation.cpp
+++ src/engine/simulation.cpp
@@ -1,4 +1,7 @@
 #include "engine/simulation.hpp"
+#include <fstream>
+#include <iostream>
+#include <QPointF>
 
 
 Simulation::Simulation()
@@ -171,4 +174,163 @@ double Simulation::getTimeToNextStep()
 {
     // TODO
     return 0.5;
+}
+
+void Simulation::readFromFile(std::string path)
+{
+   reset();
+   SimulationInstance simulationInstance;
+   std::string line;
+   int lineNo = 0;
+   int stationsNo = 0;
+   
+   std::ofstream confFile (path);
+   if(confFile.is_open())
+   {
+       while (std::getline(confFile, line) )
+       {
+	 if (lineNo == 0)
+	 {
+	     unsigned comaPos = 0;
+	     unsigned underlinePos = 0;
+	     unsigned spacePos = 0;
+	     
+	     if (line[0] == "C")
+	     {
+	         comaPos = line.find(",");
+		 simulationInstance.arrivalTimeDistribution.type = DistributionType::Constant;
+		 simulationInstance.arrivalTimeDistribution.param1 = atof(line.substr(1, comaPos-1));
+	     }
+	     if (line[0] == "U")
+	     {
+	       underlinePos = line.find("_");
+	       comaPos = line.find(",");
+	       simulationInstance.arrivalTimeDistribution.type = DistributionType::Uniform;
+	       simulationInstance.arrivalTimeDistribution.param1 = atof(line.substr(1, underlinePos-1));
+	       simulationInstance.arrivalTimeDistribution.param2 = atof(line.substr(underlinePos+1, comaPos-underlinePos));
+	     }
+	     if (line[0] == "E")
+	     {
+	        comaPos = line.find(",");
+		 simulationInstance.arrivalTimeDistribution.type = DistributionType::Exponential;
+		 simulationInstance.arrivalTimeDistribution.param1 = atof(line.substr(1, comaPos-1));
+	     }
+	     if (line[0] == "N")
+	     {
+	       underlinePos = line.find("_");
+	       comaPos = line.find(",");
+	       simulationInstance.arrivalTimeDistribution.type = DistributionType::Normal;
+	       simulationInstance.arrivalTimeDistribution.param1 = atof(line.substr(1, underlinePos-1));
+	       simulationInstance.arrivalTimeDistribution.param2 = atof(line.substr(underlinePos+1, comaPos-underlinePos));
+	     }
+	     
+	     spacePos = line.find(" ");
+	     stationsNo = atoi(line.substr(spacePos+1, line.length()-spacePos));
+	 }
+	 
+	 else if(lineNo <= stationsNo)
+	 {
+	     //add new station
+	     Station newStation = Station();
+	     loadDistribution(line, newStation);
+	     
+	     float posX = 0.0;
+	     float posY = 0.0;
+	     unsigned comaPos = line.find(",");
+	     newStation.processorCount = atoi(line[comaPos+1]);
+	     if (line.substr(comaPos+3, 4) == "FIFO")
+	     {
+	         newStation.queueType = QueueType::Fifo;
+	     }
+	     else
+	     {
+		 newStation.queueType = QueueType::Random;
+	     }
+	     line = line.substr(comaPos+8);
+	     comaPos = line.find(",");
+	     newStation.queueLength = atoi(line.substr(0, comaPos));
+	     line = line.substr(comaPos+1);
+	     comaPos = line.find(",");
+	     posX = atof(line.substr(0, comaPos));
+	     posY = atof(line.substr(comaPos+1, line.length()-comaPos));
+	     QPointF point(posX, posY);
+	     newStation.position = point;
+	     
+	     simulationInstance.stations.addStation(newStation);
+	 }
+	 else if(lineNo > stationsNo)
+	 {
+	      //add new connection
+	      //0 - wejście z całego systemu
+	      //stanowiska numerujemy od 1 do n
+	      //n+1 - wyjście z całego systemu
+	      Connection newCon = Connection();
+	      unsigned comaPos = line.find(",");
+	      if (line[0] == "W")
+	      {
+		  newCon.from = 0;
+	      }
+	      else
+	      {
+		  newCon.from = atoi(line.substr(0, comaPos);
+	      }
+	      line = line.substr(comaPos+1);
+	      comaPos = line.find(",");
+	      if (line[0] == "W")
+	      {
+		  newCon.to = stationsNo+1;
+	      }
+	      else
+	      {
+		  newCon.to = atoi(line.substr(0, comaPos);
+	      }
+	      newCon.weight = atoi(line.substr(comaPos+1, line.length()-comaPos));
+	      
+	      simulationInstance.connections.addConnection(newCon);	   
+	 }
+	 lineNo++;  
+       }
+       confFile.close();
+   }
+}
+
+void Simulation::saveToFile()
+{
+    
+  
+}
+
+void loadDistribution(std::string line, Station& station)
+{
+     unsigned comaPos = 0;
+     unsigned underlinePos = 0;
+	     
+	     if (line[0] == "C")
+	     {
+	         comaPos = line.find(",");
+		 station.serviceTimeDistribution.type = DistributionType::Constant;
+		 station.serviceTimeDistribution.param1 = atof(line.substr(1, comaPos-1));
+	     }
+	     if (line[0] == "U")
+	     {
+	       underlinePos = line.find("_");
+	       comaPos = line.find(",");
+	       station.serviceTimeDistribution.type = DistributionType::Uniform;
+	       station.serviceTimeDistribution.param1 = atof(line.substr(1, underlinePos-1));
+	       station.serviceTimeDistribution.param2 = atof(line.substr(underlinePos+1, comaPos-underlinePos));
+	     }
+	     if (line[0] == "E")
+	     {
+	         comaPos = line.find(",");
+		 station.serviceTimeDistribution.type = DistributionType::Exponential;
+		 station.serviceTimeDistribution.param1 = atof(line.substr(1, comaPos-1));
+	     }
+	     if (line[0] == "N")
+	     {
+	       underlinePos = line.find("_");
+	       comaPos = line.find(",");
+	       station.serviceTimeDistribution.type = DistributionType::Normal;
+	       station.serviceTimeDistribution.param1 = atof(line.substr(1, underlinePos-1));
+	       station.serviceTimeDistribution.param2 = atof(line.substr(underlinePos+1, comaPos-underlinePos));
+	     }
 }
